b92eb7efb065912946db3c715b55b5b1af3bcb9f
Iain Barkley <ibarkely@tektelic.com>
2023-02-19 18:42:39 -0500

    Renamed 'Appgraphics' module to 'appgraphics' module

--- components/AppGraphics/src/AppGraphics.h
+++
@@ -1,9 +0,0 @@
-#include <u8g2.h>
-
-void AppGraphicsPrintDroplet(void);
-
-void AppGraphicsPrintOnOled(int text, int height);
-
-void AppGraphicsHandleText(int text);
-
-void AppGraphicsHandleGraphics(int text);
